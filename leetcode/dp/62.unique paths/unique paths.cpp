64ad67ef2e6843ccf52a0d9a00e40422a434b3ea
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-02 08:28:50 +0100

    unique path memoization

--- leetcode/dp/62.unique paths/unique paths.cpp
+++ leetcode/dp/62.unique paths/unique paths.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int dp[101][101];
 
 int uniquePaths(int n,int m) {
+    if (dp[m][n] != -1 ) return dp[m][n];
     if (n == 1 && m == 1) return 1;
     if (n == 0 || m == 0) return 0;
-    return uniquePaths(n-1,m)+uniquePaths(n,m-1);
+    dp[m][n] = uniquePaths(n-1,m)+uniquePaths(n,m-1);
+    return dp[m][n];
 }
 
 
 int main() {
+    memset(dp,-1,sizeof(dp));
     int n,m;
     cin >> n;
     cin >> m;
