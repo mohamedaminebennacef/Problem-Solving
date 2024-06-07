876b73e4694614a36bae00b9ee5ff94b2ff1d9f1
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-07 17:09:23 +0100

    767. Reorganize String

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
-
 int main() {
-    vector<int> hand = {4,1,2,3,6,2,3,4,7,8};
-    for(auto element : hand)
-        cout << element << ",";
-    cout << endl;
-    for(int i=0;i<hand.size();i++)
-        cout << hand[i]<< ",";;
-
+    priority_queue<pair<int,int>> pq;
+    pq.push({1,2});
+    pq.push({3,4});
+    pq.push({5,6});
+    while (!pq.empty()) {
+        pair<int,int> topElement = pq.top();
+        cout << topElement.first;
+        cout << topElement.second;
+        pq.pop();
+    }
     return 0;
 }
