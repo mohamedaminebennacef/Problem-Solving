e8ab4763b1a71d80992669d3807633dd83e3a610
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-10 17:53:24 +0100

    36. Valid Sudoku

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,16 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <map>
+#include <vector>
+#include <utility> // For std::pair
+
 int main() {
-    priority_queue<pair<int,int>> pq;
-    pq.push({1,2});
-    pq.push({3,4});
-    pq.push({5,6});
-    while (!pq.empty()) {
-        pair<int,int> topElement = pq.top();
-        cout << topElement.first;
-        cout << topElement.second;
-        pq.pop();
+    // Define the map: key is an int, value is a vector of pairs of ints
+    std::map<int, std::vector<std::pair<int, int>>> myMap;
+
+    // Insert an empty vector for a key if it doesn't exist
+    int key = 1;
+    if (myMap.find(key) == myMap.end()) {
+        myMap[key] = std::vector<std::pair<int, int>>();
     }
-    cout << "test";
+
+    // Add a new pair to the vector associated with the key
+    std::pair<int, int> newPair = std::make_pair(10, 20);
+    myMap[key].push_back(newPair);
+
+    // Display the contents of the map
+    for (const auto& entry : myMap) {
+        std::cout << "Key: " << entry.first << "\n";
+        for (const auto& pair : entry.second) {
+            std::cout << "  Pair: (" << pair.first << ", " << pair.second << ")\n";
+        }
+    }
+
     return 0;
 }
