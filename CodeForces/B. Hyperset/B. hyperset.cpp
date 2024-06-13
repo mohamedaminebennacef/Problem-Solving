8ce248eb8b537b1a44e2fb5a3847f0f9b2943fb7
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-13 11:12:53 +0100

    Revert "B. Hyperset TLE Solution1"
    This reverts commit d458886c85aa02365d36a2e0a369764f9b16bde7.

--- CodeForces/B. Hyperset/B. hyperset.cpp
+++ CodeForces/B. Hyperset/B. hyperset.cpp
@@ -1,49 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int i,n,k,r;string t;set<string>s;
+int main(){for(cin>>n>>k;n--;){cin>>t;
+for(auto x:s){for(i=k;i--;)if(x[i]^t[i])x[i]^=66^t[i];r+=s.count(x);}
+s.insert(t);}cout<<r/2;}
 
-int main() {
-    int n=3,res=0;vector<string> v = {"set","ets","tse"};
-    for(int i=0;i<n-2;i++){
-        for(int j=i+1;j<n-1;j++){
-            for(int x=j+1;x<n;x++) {
-                bool cut=false;string s1=v[i];string s2=v[j];string s3=v[x];                
-                for(int a=0;a<n;a++){
-                    if ( s1[a]==s2[a] && s1[a]!=s3[a] || s1[a]==s3[a] && s1[a]!=s2[a] || s2[a]==s3[a] && s1[a]!=s2[a] ){
-                        cut=true;
-                        break;
-                    }
-                }
-                if(!cut) {
-                    res++;
-                }
-            }
-        }
-    }
-    cout << res << endl;
-    return 0;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// int i,n,k,r; // Declare variables i, n, k, r as integers
+// string t; // Declare t as a string
+// set<string>s; // Declare s as a set of strings
 
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int n=3,res=0;vector<string> v = {"set","ets","tse"};
-    for(int i=0;i<n-2;i++){
-        for(int j=i+1;j<n-1;j++){
-            for(int x=j+1;x<n;x++) {
-                bool cut=false;string s1=v[i];string s2=v[j];string s3=v[x];                
-                for(int a=0;a<n;a++){
-                    if ( s1[a]==s2[a] && s1[a]!=s3[a] || s1[a]==s3[a] && s1[a]!=s2[a] || s2[a]==s3[a] && s1[a]!=s2[a] ){
-                        cut=true;
-                        break;
-                    }
-                }
-                if(!cut) {
-                    res++;
-                }
-            }
-        }
-    }
-    cout << res << endl;
-    return 0;
-}
+// int main() {
+//     for(cin >> n >> k; n--;){ // Read n and k, then loop n times
+//         cin >> t; // Read the string t
+//         for(auto x : s) { // Loop over each string x in the set s
+//             for(i = k; i--;) // Loop k times in reverse order (i goes from k-1 to 0)
+//                 if(x[i] ^ t[i]) // If characters at position i in x and t are different
+//                     x[i] ^= 66 ^ t[i]; // Modify x[i] by XORing it with 66 and t[i]
+//             r += s.count(x); // Increment r by 1 if the modified x is in the set s
+//         }
+//         s.insert(t); // Insert t into the set s
+//     }
+//     cout << r/2; // Print r divided by 2
+// }
