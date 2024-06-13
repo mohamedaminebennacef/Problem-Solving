14788dfa1193d3aefa4c3573f773f55e4cfa2270
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-13 11:15:07 +0100

    B. Hyperset Solution1

--- CodeForces/B. Hyperset/B. hyperset.cpp
+++ CodeForces/B. Hyperset/B. hyperset.cpp
@@ -1,26 +1,39 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-int i,n,k,r;string t;set<string>s;
-int main(){for(cin>>n>>k;n--;){cin>>t;
-for(auto x:s){for(i=k;i--;)if(x[i]^t[i])x[i]^=66^t[i];r+=s.count(x);}
-s.insert(t);}cout<<r/2;}
-
-// #include<bits/stdc++.h>
-// using namespace std;
-// int i,n,k,r; // Declare variables i, n, k, r as integers
-// string t; // Declare t as a string
-// set<string>s; // Declare s as a set of strings
-
-// int main() {
-//     for(cin >> n >> k; n--;){ // Read n and k, then loop n times
-//         cin >> t; // Read the string t
-//         for(auto x : s) { // Loop over each string x in the set s
-//             for(i = k; i--;) // Loop k times in reverse order (i goes from k-1 to 0)
-//                 if(x[i] ^ t[i]) // If characters at position i in x and t are different
-//                     x[i] ^= 66 ^ t[i]; // Modify x[i] by XORing it with 66 and t[i]
-//             r += s.count(x); // Increment r by 1 if the modified x is in the set s
-//         }
-//         s.insert(t); // Insert t into the set s
-//     }
-//     cout << r/2; // Print r divided by 2
-// }
+ 
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<string> cards(n);
+    
+    for(int i = 0; i < n; ++i) {
+        cin >> cards[i];
+    }
+    
+    int count = 0;
+ 
+    for(int i = 0; i < n; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+            for(int l = j + 1; l < n; ++l) {
+                bool isSet = true;
+                for(int m = 0; m < k; ++m) {
+                    char a = cards[i][m];
+                    char b = cards[j][m];
+                    char c = cards[l][m];
+                    if (!((a == b && b == c) || (a != b && b != c && a != c))) {
+                        isSet = false;
+                        break;
+                    }
+                }
+                if (isSet) {
+                    ++count;
+                }
+            }
+        }
+    }
+    
+    cout << count << endl;
+    return 0;
+}
