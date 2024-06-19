446763b82ab2d615b1abe72f188382eb9cc5c4e9
mohamed bennacef <aminebennacef098@gmail.com>
2024-06-19 08:40:24 +0100

    1514 B. AND 0, Sum Big | Codeforces

--- testing/test.cpp
+++ testing/test.cpp
@@ -140,11 +140,13 @@ int main()
     //     asquare();
     // }
     // return 0;
-  int x=56;
-  for (int i = 31; i >= 0; i--) {
-    if (x&(1<<i)) cout << "1";
-    else cout << "0";
-  }
+  // int x=56;
+  // for (int i = 31; i >= 0; i--) {
+  //   if (x&(1<<i)) cout << "1";
+  //   else cout << "0";
+  // }
+  int n=2,k=2;
+  cout<<pow(n,k)%10;
 
 }
 //End
