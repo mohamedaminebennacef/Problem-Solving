f16cdd98c332bc2e2da4c345d1ee2c4e384c655b
mohamed bennacef <aminebennacef098@gmail.com>
2024-07-01 15:39:17 +0100

    1547B Alphabetical Strings Solution2 using Two Pointers

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    string s2="";
-    for(int i=0;i<8;i++) {
-        cout<<char('a'+i)<<' ';
-    }
+    char c='a'+1;
+    cout<<c;
     return 0;
 }
