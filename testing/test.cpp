7a36d6e65ba31058a8e702b330206837567eab79
mohamed bennacef <aminebennacef098@gmail.com>
2024-07-01 14:21:00 +0100

    1547B Alphabetical Strings Solution1

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,31 +1,9 @@
-#include <iostream>
-#include <map>
-
-int mostFrequentElement(const std::map<int, int>& freqMap) {
-    int maxCount = 0;
-    int mostFrequent = freqMap.begin()->first; // Initialize with the first element
-
-    // Iterate through the map to find the element with the maximum frequency
-    for (const auto& pair : freqMap) {
-        if (pair.second > maxCount) {
-            maxCount = pair.second;
-            mostFrequent = pair.first;
-        }
-    }
-
-    return mostFrequent;
-}
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    // Example map where keys are elements and values are their frequencies
-    std::map<int, int> freqMap = {
-        {1, 3},
-        {2, 4},
-        {3, 1},
-        {4, 2}
-    };
-
-    int result = mostFrequentElement(freqMap);
-    std::cout << "The most frequent element is: " << result << std::endl;
+    string s2="";
+    for(int i=0;i<8;i++) {
+        cout<<char('a'+i)<<' ';
+    }
     return 0;
 }
