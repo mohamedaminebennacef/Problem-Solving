195023354bb655b474280066f025fc6fcae64845
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-10 22:46:27 +0100

    1598. Crawler Log Folder Daily Challenge constant space O(1) Linear Time O(n)

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    char c='a'+1;
-    cout<<c;
+    vector<string> logs = {"d1/","d2/","./","d3/","../","d31/"};
+                        // = {"d1","d1/d2","d1/d2","d1/d2/d3","d1/d2","d1/d2/d31"} => ../ (d2) ../ (d1) ../ (Main)
+    int n=logs.size();
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(logs[i] == "../") {
+            if (ans>0)
+                ans--;
+        }
+        else if (logs[i] == "./")
+            continue;
+        else {
+            ans++;
+        }
+    }
+    cout<<ans;
     return 0;
 }
