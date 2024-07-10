7835bd7f2d946fb7f411ed41bf4d5cffad9b2767
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-10 14:33:37 +0100

    1462F The Treasure of The Segments Solution1 O(t*n^2) TLE

--- Data Structures and Sorting/75. Sort colors/75. Sort colors.cpp
+++ Data Structures and Sorting/75. Sort colors/75. Sort colors.cpp
@@ -1,23 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-// class Solution {
-// public:
-//     void sortColors(vector<int>& nums) {
-//         map<int,int> freq(0,3);
-//         for(int i=0;i<nums.size();i++) {
-//             freq[nums[i]]++;
-//         }
-//         int x = 0;
-//         for(int i=0;i<nums.size();i++) {
-//             for(int j=i;j<freq[x];j++) {
-//                 nums[i]
-//             }
-//         }
-//     }
-// };
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    // void sortColors(vector<int>& nums) {
+    //     map<int,int> freq(0,3);
+    //     for(int i=0;i<nums.size();i++) {
+    //         freq[nums[i]]++;
+    //     }
+    //     int x = 0;
+    //     for(int i=0;i<nums.size();i++) {
+    //         for(int j=i;j<freq[x];j++) {
+    //             nums[i]
+    //         }
+    //     }
+    // }
+    void sortColors2(vector<int>& nums) {
         int low = 0;
         int mid = 0;
         int high = nums.size()-1;
@@ -40,7 +37,27 @@ public:
             }
         }
     }
+    void sortColors3(vector<int>& nums) {
+        int zeros = 0, ones = 0, n = nums.size();
+        for(int num : nums) {
+            if(num == 0) zeros++;
+            else if(num == 1) ones++;
+        }  
+
+        for(int i = 0; i < zeros; ++i) {
+            nums[i] = 0;
+        }
+
+        for(int i = zeros; i < zeros + ones; ++i) {
+            nums[i] = 1;
+        }
+
+        for(int i = zeros + ones; i < n; ++i) {
+            nums[i] = 2;
+        }
+    }
 };
+
 
 int main() {
 
