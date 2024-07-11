605f6ed84d5b58da4d8a6065e5ec43438613a3e6
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-11 11:09:50 +0100

    1428C ABBB Solution2 using Stack

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,19 +1,7 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    std::string str = "ABBABA";
-    std::string to_remove = "AB";
-    
-    // Find the starting position of the substring to remove
-    size_t pos = str.find(to_remove);
-    
-    // If the substring is found, remove it
-    if (pos != std::string::npos) {
-        str.erase(pos, to_remove.length());
-    }
-
-    std::cout << "Resulting string: " << str << std::endl;
-
+    stack<char> st;
+    cout<<st.top();
     return 0;
 }
