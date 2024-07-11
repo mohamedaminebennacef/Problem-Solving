c3cdc35c5cb4b150628ce03bd59b990575af4630
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-11 20:32:16 +0100

    1353D Constructing the Array Solution1 TLE

--- Chapter 4 Data structures/Set structures/set.cpp
+++ Chapter 4 Data structures/Set structures/set.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int main() {
     set<int> s;
     s.insert(3);
-    s.insert(2);
+    s.insert(2); 
     s.insert(5);
-    s.insert(5);
+    s.insert(5); // no duplicate
 
-    for (set<int>::iterator it=s.begin();it!=s.end();it++) cout << *it << "\n";
+    for (set<int>::iterator it=s.begin();it!=s.end();it++) cout << *it << "\n"; // 2 3 5 (sorted)
 
     return 0;
 }
