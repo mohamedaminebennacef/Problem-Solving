3898bf8e606496b000b195ca5f5b9a0390d5758e
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-13 09:24:40 +0100

    402. Remove K Digits using Deque

--- testing/test.cpp
+++ testing/test.cpp
@@ -1,11 +1,20 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    deque<int> d = {1,2,3,4};
-    for(int i=0;i<4;i++) {
-        cout<<d[i]<<" ";
-    }
 
+int f(vector<int>& v,int n,int m) {
+    if (n<0) 
+        return m;
+    if (m<v[n])
+        return f(v,n-1,v[n]);
+    else    
+        return f(v,n-1,m);
+}
+
+int main() {
+    int m=INT_MIN;
+    vector<int> v = {2,4,-1,0,9,-1,-1000};
+    int n=v.size();
+    int x=f(v,n-1,m);
+    cout<<x;
     return 0;
 }
