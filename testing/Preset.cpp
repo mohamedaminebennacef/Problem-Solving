b4c12f017e331ffc5e7d7bcb1cd99e52052b7093
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-14 15:51:36 +0100

    1341B Nastya and Door Using Cumulative Sum

--- testing/Preset.cpp
+++ testing/Preset.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
-#define FAST ios_base::sync_with_stdio(false):cin.tie(NULL):cout.tie(NULL);
-#define newL cout<<"\n";
+#define FAST ios_base::sync_with_stdio(false):cin.tie(NULL):cout.tie(NULL)
+#define newL cout<<"\n"
+#define ll long long
+#define pb push_back
+#define f first
 using namespace std;
 int main() {
     freopen("input.txt","r",stdin);
@@ -9,17 +12,5 @@ int main() {
     while(t--) {
 
     }
-    return 0;
-}
-#include <bits/stdc++.h>
-using namespace std;
-int n;
-int main() {
-    freopen("input.txt","r",stdin);
-    cin>>n;
-    vector<int> v(n);
-    for(int i=0;i<n;i++) cin>>v[i];
-       
-    //cout<<ans<<endl; 
     return 0;
 }
