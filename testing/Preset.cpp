2af0fe4eee222fe8f1fe8f3f926b6eb074762ea3
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-14 13:19:00 +0100

    313B Ilya and Queries Solution1 TLE

--- testing/Preset.cpp
+++ testing/Preset.cpp
@@ -2,17 +2,13 @@
 #define FAST ios_base::sync_with_stdio(false):cin.tie(NULL):cout.tie(NULL);
 #define newL cout<<"\n";
 using namespace std;
-int t,n;
 int main() {
     freopen("input.txt","r",stdin);
+    int t;
     cin>>t;
     while(t--) {
-        cin>>n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) cin>>v[i];
-       
+
     }
-    //cout<<ans<<endl; 
     return 0;
 }
 #include <bits/stdc++.h>
