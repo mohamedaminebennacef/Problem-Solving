d6f6d0fd918a86cf2a7c6f8dc7d0133cb451ce6d
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-17 14:05:14 +0100

    1985D Manhattan Circle 3 Solution Math

---
+++ Preset/Preset.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    freopen("input.txt","r",stdin); 
+    
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int T=1;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+}
+
+
+/*
+  // think greedy first, codeforces A,B usually some sort of greedy, A super simple usually (800)
+  // C on Div2 or E on Div3/4 require more thinking but still not too hard
+  // for later problems, think about data structures like segment tree and ordered set
+  // try query problems, im oddly good at them
+  // As a participant, I approve these statements.
+*/
