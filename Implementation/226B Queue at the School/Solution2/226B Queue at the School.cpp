c602c6cdb9e7f2577ec68a7359f026a1a45d8d2f
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-18 14:08:19 +0100

    226B Queue at the School Read and Understand the Problem Solution1 O(t*n) Time 92ms

--- Implementation/226B Queue at the School/Solution2/226B Queue at the School.cpp
+++ Implementation/226B Queue at the School/Solution2/226B Queue at the School.cpp
@@ -4,13 +4,13 @@ signed main() {
     freopen("../input.txt","r",stdin);
     int n,t;string s;
     cin>>n>>t>>s;
+    string s2=s;
     while(t--) {
         for(int i=0;i<n-1;i+=1) {
-            if(s[i]=='B'&& s[i+1]=='G') {
+            if(s2[i]=='B'&& s2[i+1]=='G')
                 swap(s[i],s[i+1]);
-                i++;
-            }
         }
+        s2=s;
     }
     cout<<s<<endl;
     // GBGGB
