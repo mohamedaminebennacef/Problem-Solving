f1c69bce5ae3a77248e79d4fa48b6223110b7af6
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-07-21 11:49:58 +0100

    1990A Submission Bait Solution

--- Preset/Preset.cpp
+++ Preset/Preset.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 
-signed main() {
-  ios_base::sync_with_stdio(false); cin.tie(NULL);
-  freopen("input.txt","r",stdin);
-  int T=1;cin >> T;
-  while (T--) {
-  
-  }
+void solve() {
 }
 
+int main()
+{
+    #ifdef SOMETHING
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif // SOMETHING
+    ios_base::sync_with_stdio(false), cin.tie(0);
 
+    int tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
 /*
   // think greedy first, codeforces A,B usually some sort of greedy, A super simple usually (800)
   // C on Div2 or E on Div3/4 require more thinking but still not too hard
