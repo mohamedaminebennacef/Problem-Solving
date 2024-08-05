294d100de79f885dcfbd13e8b585f44297b72c56
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-08-05 11:46:42 +0100

    Indexes Of Subarray Sum Solution2 O(n^2) using Two Pointers (Inefficient,Unecessary Reset of the Window)

--- Two Pointers/Indexes Of Subarray Sum/Solution3/Indexes Of Subarray Sum.cpp
+++ Two Pointers/Indexes Of Subarray Sum/Solution3/Indexes Of Subarray Sum.cpp
@@ -14,34 +14,46 @@ using namespace std;
 
 // } Driver Code Ends
 
-
 class Solution {
   public:
     // Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int> arr, int n, long long s) {
-        vector<int> indices;
-        int start=0,end=0;
-        long long ans=arr[start];
-        while(end<n) {
-            if (ans<s) {
-                end++;
-                ans+=arr[end];
-            }
-            else if (ans==s) {
-                indices.push_back(start+1);
-                indices.push_back(end+1);
+        vector<int> indices(1,-1);
+        if (!s) {
+            if (find(arr.begin(),arr.end(),0) != arr.end()){
+                indices[0] = find(arr.begin(),arr.end(),0)-arr.begin()+1;
+                indices.push_back(find(arr.begin(),arr.end(),0)-arr.begin()+1);
                 return indices;
             }
-            else {
+            else
+                return indices;
+        }
+        int start=0,end=1;
+        long long sm=arr[start];
+        if (sm == s) {
+            indices[0]=1;
+            indices.push_back(1);
+            return indices;
+        }
+        while(end<n) {
+            if (sm+arr[end]>s) {
+                sm-=arr[start];
                 start++;
-                end=start;
-                ans=arr[start];
+            }
+            else {
+                sm+=arr[end];
+                end++;
+            }
+            if (sm==s) {
+                indices[0]=start+1;
+                indices.push_back(end);
+                return indices;
             }
         }
-        indices.push_back(-1);
         return indices;
     }
 };
+
 
 //{ Driver Code Starts.
 
