5ac14db75781deaaaa0f382787990f19ca72d214
mohamedaminebennacef <aminebennacef098@gmail.com>
2024-08-15 20:17:18 +0100

    1679. Max Number of K-Sum Pairs Solution3 O(n) Using frequency map.

--- Chapters/Chapter 4 Data structures/Map structures/Map/map.cpp
+++ Chapters/Chapter 4 Data structures/Map structures/Map/map.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+elements are sorted (ordered)
+no duplicate key No two mapped values can have the same key
+*/
+
 int main()  {
     map<int,int> mp;
     mp[5] = 1;
@@ -8,9 +13,9 @@ int main()  {
     mp[0] = 9;
     mp[7] = 1;
 
-    // for(auto i:mp) {
-    //     cout << i.first << " " << i.second << "\n";
-    // }
+    for(auto i:mp) {
+        cout << i.first << " " << i.second << "\n";
+    }
 
     auto element = mp.begin();
     cout << element->first << " " << element->second;
--- testing/Preset.cpp
+++ testing/Preset.cpp
@@ -1,48 +1,37 @@
 #include "bits/stdc++.h"
-
-#define all(a) a.begin(), a.end()
-#define pb push_back
-typedef long long ll;
 using namespace std;
  
-/// Actual code starts here
-const int N = 100005;
- 
 void solve() {
-    ll x;
-    cin >> x;
-    vector<int> res(31, 0);
-    for (int i = 0; i < 30; i++) {
-        if (1ll & (x >> i)) {
-            if (res[i]) {
-                res[i + 1] = 1;
-                res[i] = 0;
-            } else if (i > 0) {
-                if (res[i - 1] == 1) {
-                    res[i + 1] = 1;
-                    res[i - 1] = -1;
-                } else {
-                    res[i] = 1;
+    freopen("input.txt","r",stdin);
+    int t;cin>>t;
+    while(t--) {
+        int n,k;cin>>n>>k;
+        vector<int> a(n);
+        for(auto& i:a) cin>>i;
+        sort(a.begin(),a.end(),greater<int>());
+        long long A=0,B=0,score=0;
+        for(int i=0;i<n-1;i++) {
+            if (k!=0) {
+                if (a[i]-a[i+1]>=0) {
+                    int x=a[i+1];
+                    a[i+1]+=min(k,a[i]-a[i+1]);
+                    k-=min(k,a[i]-x);
+                    i++;
                 }
-            } else if (i == 0) {
-                res[i] = 1;
             }
+            else {
+                break;
+            } 
         }
+        for(int i=0;i<n;i++) {
+            if (i%2==0)
+                A+=a[i];
+            else
+                B+=a[i]; 
+        }
+        score=A-B;
+        cout<<score<<endl;
     }
-    cout << 31 << '\n';
-    for (int i = 0; i <= 30; i++) {
-        cout << res[i] << ' ';
-    }
-    cout << '\n';
 }
  
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-	freopen("input.txt","r",stdin);
-    int tt = 1;
-    cin >> tt;
-    while (tt--)
-        solve();
-}
+signed main() {solve();}
