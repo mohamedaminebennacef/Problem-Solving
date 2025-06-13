fa31cb1958c496129540c0b8f9c9e35e3e3bb8cb
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-06-13 11:34:25 +0100

    basic tcpc template

---
+++ tcpc.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define f first
+#define s second
+#define sz(a) (int)(a.size())
+
+void solve()
+{
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
