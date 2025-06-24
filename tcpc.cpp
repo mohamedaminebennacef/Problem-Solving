ea2d71946a8057810fbf704d2c1d2dc45aa7fbd4
mohamedaminebennacef <aminebennacef098@gmail.com>
2025-06-24 13:45:47 +0100

    tcpc file update add ios::sync_with_studio(0); cin.tie(0); cout.tie(0); for more input and output efficiency

--- tcpc.cpp
+++ tcpc.cpp
@@ -14,6 +14,9 @@ bool multi = true;
 
 signed main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
